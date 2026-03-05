3fb75c8e015890fb53e80cc64b0c4dffea535574
ArthurSS-DevOps <vmsdotrabalho@gmail.com>
2026-03-05 09:30:29 -0300

    S3 completo em fase de contrução

--- main.tf
+++ main.tf
@@ -29,3 +29,22 @@ resource "aws_s3_bucket_public_access_block" "frontend" {
   block_public_policy     = false
   restrict_public_buckets = false
 }
+resource "aws_s3_bucket_policy" "frontend_policy" {
+  bucket = aws_s3_bucket.frontend.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.frontend.arn}/*"
+    }]
+  })
+}
+resource "aws_s3_object" "index.html" {
+  bucket       = aws_s3_bucket.frontend.id
+  key          = "index.html"
+  source       = "frontend/index.html"
+  content_type = "text/html"
+}
