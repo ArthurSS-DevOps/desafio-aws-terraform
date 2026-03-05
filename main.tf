a9aa641d6ccf39958ab51a7e2ca29422fbfd1576
ArthurSS-DevOps <vmsdotrabalho@gmail.com>
2026-03-05 09:32:19 -0300

    S3 corrigido

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ resource "aws_s3_bucket" "frontend" {
 }
 
 resource "aws_s3_bucket_website_configuration" "frontend_config" {
-  buket = aws_s3_bucket.frontend.id
+  bucket = aws_s3_bucket.frontend.id
 
   index_document {
-    sufix = "index.html"
+    suffix = "index.html"
   }
 }
 
@@ -42,7 +42,7 @@ resource "aws_s3_bucket_policy" "frontend_policy" {
     }]
   })
 }
-resource "aws_s3_object" "index.html" {
+resource "aws_s3_object" "index" {
   bucket       = aws_s3_bucket.frontend.id
   key          = "index.html"
   source       = "frontend/index.html"
