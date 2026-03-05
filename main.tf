605529a991e0e5339c6e4561edd7547a1e999bab
ArthurSS-DevOps <vmsdotrabalho@gmail.com>
2026-03-05 10:06:34 -0300

    IAM Configurado para o lambda

--- main.tf
+++ main.tf
@@ -115,3 +115,24 @@ resource "aws_instance" "backend" {
   }
 }
 
+#############################
+# IAM ROLE LAMBDA
+#############################
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-role-${random_id.rand.hex}"
+
+  assume_role_policy = jsonencode({
+
+    Version = "2012-10-17"
+    Statement = [{
+
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Sercive = "lambda.amazonaws.com"
+      }
+    }]
+    
+    })
+}
